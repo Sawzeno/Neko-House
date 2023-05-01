edfa533c446985306ca4cf6f840d05b42094b78a
gon araseon <72021422+ravikul505@users.noreply.github.com>
2023-05-01 20:55:57 +0530

    premature optimization
    cope

--- Assets/2.Scripts/BoardCreator.cs
+++ Assets/2.Scripts/BoardCreator.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class BoardCreator : MonoBehaviour
@@ -26,6 +27,9 @@ public class BoardCreator : MonoBehaviour
     //Game Object List
     private GameObject[,] _board;
     private int[,] _life;
+
+    //constants
+
 
     private void CreateBoard()
     {
@@ -97,9 +101,10 @@ public class BoardCreator : MonoBehaviour
         var ray = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out var hit))
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.C))
             {
                 Debug.Log("_______" + hit.transform.tag);
+                hit.transform.position += new Vector3(0, 0.5f, 0);
 
                 switch (hit.transform.tag)
                 {
@@ -144,46 +149,113 @@ public class BoardCreator : MonoBehaviour
         }
     }
 
+
     private void PawnMoves(RaycastHit obj, string type)
     {
+        //possible moves for pawn
+
+        Vector3 t = obj.transform.position;
+        int I; //initial
+        int D; //Direction
+
+        //white
+        if (type == "White Pawn")
+        {
+            I = 1;
+            D = 1;
+        }
+        //black
+        else
+        {
+            I = 6;
+            D = -1;
+        }
+
+        if ((int)t.z == I)
+        {
+            Colour((int)t.x, (int)t.z + D, green);
+
+            Colour((int)t.x, (int)t.z + D * 2, green);
+        }
+        else if (t.z + 1 < 8)
+
+        {
+            if (_life[(int)t.x, (int)t.z + 1] == 0)
+            {
+                Colour((int)t.x, (int)t.z + D, green);
+            }
+
+            if (t.x + 1 < 8)
+            {
+                if (_life[(int)t.x + 1, (int)t.z + 1] == 1)
+                {
+                    Colour((int)t.x + 1, (int)t.z + D, red);
+                }
+            }
+
+            if (t.x - 1 >= 0)
+            {
+                if (_life[(int)t.x - 1, (int)t.z + 1] == 1)
+                {
+                    Colour((int)t.x - 1, (int)t.z + D, red);
+                }
+            }
+        }
     }
 
     private void RookMoves(RaycastHit obj, string type)
     {
         //possible moves for rook
+        Vector3 t = obj.transform.position;
+
+        for (int j = (int)t.z + 1; j < 8; j++)
+        {
+            int l = _life[(int)t.x, (int)t.z + j] + _life[(int)t.x, (int)t.z];
+            if (l == 0)
+            {
+                Colour((int)t.x, j, red);
+            }
+
+            if (l == 1)
+            {
+                Colour((int)t.x, j, green);
+                return;
+            }
+        }
     }
 
     private void KnightMoves(RaycastHit obj, string type)
     {
         Vector3 t = obj.transform.position;
-        
+
         //possible moves for knight
         for (int i = -2; i <= 2; i++)
         {
-            for(int j = -2 ;j<= 2;j++ )
+            for (int j = -2; j <= 2; j++)
             {
+                int l = _life[(int)t.x + i, (int)t.z + j] + _life[(int)t.x, (int)t.z];
                 if (i == 0 || j == 0 || i == j || i == -j)
                 {
                     continue;
                 }
+
                 if (t.x + i < 0 || t.x + i > 7 || t.z + j < 0 || t.z + j > 7)
                 {
                     continue;
                 }
 
-                int l = _life[(int)t.x + i, (int)t.z + j] + _life[(int)t.x , (int)t.z];
-                if (l == 1|| l == -1)
+                if (l == 1)
                 {
-                    _board[(int)t.x +i, (int)t.z + j].GetComponent<Renderer>().material = green;
+                    Colour((int)t.x + i, (int)t.z + j, green);
                 }
                 else if (l == 0)
                 {
-                    _board[(int)t.x +i, (int)t.z + j].GetComponent<Renderer>().material = red;
+                    Colour((int)t.x + i, (int)t.z + j, red);
                 }
- 
             }
         }
     }
+
 
     private void BishopMoves(RaycastHit obj, string type)
     {
@@ -198,12 +270,42 @@ public class BoardCreator : MonoBehaviour
     private void KingMoves(RaycastHit obj, string type)
     {
         //possible moves for king
+        Vector3 t = obj.transform.position;
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    continue;
+                }
+
+                if (t.x + i < 0 || t.x + i > 7 || t.z + j < 0 || t.z + j > 7)
+                {
+                    continue;
+                }
+
+                int l = _life[(int)t.x + i, (int)t.z + j] + _life[(int)t.x, (int)t.z];
+                if (l == 1 || l == -1)
+                {
+                    Colour((int)t.x + i, (int)t.z + j, green);
+                }
+                else if (l == 0)
+                {
+                    Colour((int)t.x + i, (int)t.z + j, red);
+                }
+            }
+        }
     }
 
     private void MovePieces()
     {
     }
 
+    private void Colour(int a, int b, Material type)
+    {
+        _board[a, b].GetComponent<Renderer>().material = type;
+    }
 
     private void CallInstantiation(int i, int j)
     {
@@ -217,14 +319,14 @@ public class BoardCreator : MonoBehaviour
         }
     }
 
-    private void CreateCell(int i, int j, Material input)
+    private void CreateCell(int i, int k, Material input)
     {
-        GameObject temp = Instantiate(cell, new Vector3(i, 0, j), Quaternion.identity);
+        GameObject temp = Instantiate(cell, new Vector3(i, 0, k), Quaternion.identity);
         temp.GetComponent<Renderer>().material = input;
-        temp.name = $"Cell {i} {j}";
+        temp.name = $"Cell {i} {k}";
         temp.transform.parent = transform;
-        _board[i, j] = temp;
-        _life[i, j] = 0;
+        _board[i, k] = temp;
+        _life[i, k] = 0;
     }
 
     private void CreatePiece(GameObject piece, int i, int j, int k, Quaternion rotation, string type, Material material)
@@ -245,19 +347,20 @@ public class BoardCreator : MonoBehaviour
         }
     }
 
+
     private void TestDebugger()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             for (int i = 0; i < size; i++)
             {
-                string temp =i+ " ## " + "";
+                string temp = i + " ## " + "";
                 for (int j = 0; j < size; j++)
                 {
-                    temp +=  _life[i, j] + " ";
+                    temp += _life[i, j] + " ";
                 }
-                
-                print(temp+ " ## " );
+
+                print(temp + " ## ");
             }
         }
     }
